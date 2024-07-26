ab5728583b55e1919aae649486a328ecdb4ad38e
sarthakPabbly <sarthak.tiwari@pabbly.com>
2024-07-26 11:29:49 +0530

    modified server listen

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ const server = http.createServer(async (req, res) => {
     }
 });
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// server.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
